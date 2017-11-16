e2006aef53a2e212f3f14593e3a9d55fe81b3784
Sean Waclawik <waclas@rpi.edu>
2017-11-16 15:14:50 -0500

    added json stuff

--- Code/Document.java
+++ Code/Document.java
@@ -4,10 +4,11 @@ import org.json.simple.*;
 
 class Document {
 
-
+	// holds the jason text 
 	String raw_text;
 	String strip_text;
 
+	// holds the n-grams
 	ArrayList<String> terms;
 	ArrayList<String> bigrams;
 	ArrayList<String> trigrams;
@@ -15,9 +16,25 @@ class Document {
 
 	// Constructor
 	Document(String raw) {
-
+		
+		
 		raw_text = raw;
 
+		JSONArray jFile = (JSONArray) parser.parse(new FileReader(raw));
+
+		  for (Object o : jFile)
+		  {
+		    JSONObject jObject = (JSONObject) o;
+
+		    String type = (String) jObject.get("type");
+		    //System.out.println(name);
+
+		    String link = (String) jObject.get("link");
+		    //System.out.println(city);
+
+		    String text = (String) jObject.get("text");
+		    //System.out.println(job);
+		  }
 		
 
 
