c281e4d24c1f55b108d8159057348cbcb48064c0
elawson668 <elawsont668@gmail.com>
2017-11-16 14:06:38 -0500

    Document class

---
+++ Code/Document.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Document {
+
+
+	String raw_text;
+	String strip_text;
+
+	ArrayList<String> terms;
+
+	// Constructor
+	Document() {
+
+
+
+	}
+
+
+	void remove_stop_words() {
+
+
+	}
+
+	void bigrams() {
+
+	}
+
+	void trigrams() {
+
+	}
+
+	
+
+
+
+
+}
