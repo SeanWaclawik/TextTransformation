13d1306163406e7d9899d38c4de99a2cf981beb0
elawson668 <elawsont668@gmail.com>
2017-11-18 11:36:42 -0500

    strip text

--- Code/Document.java
+++ Code/Document.java
@@ -33,18 +33,20 @@ public class Document {
 		
 		JSONArray jFile = (JSONArray) parser.parse(new FileReader(fname));
 
-		  for (Object o : jFile) {
-		    JSONObject jObject = (JSONObject) o;
-		    type = (String) jObject.get("type");
-		    link = (String) jObject.get("link");
-		    raw_text = (String) jObject.get("text");
-		  }
+		  	for (Object o : jFile) {
+		    	JSONObject jObject = (JSONObject) o;
+		    	type = (String) jObject.get("type");
+		    	link = (String) jObject.get("link");
+		    	raw_text = (String) jObject.get("text");
+		  	}
 		  
-		  boolean res = initStopWords(stopWordsLoc);  
-		  if (!res){
-			  System.err.format("Exception occered trying to read file '%s'", loc);
-			  e.printStackTrace();
-		  }
+		  	boolean res = initStopWords(stopWordsLoc);  
+		  	if (!res){
+			  	System.err.format("Exception occered trying to read file '%s'", loc);
+			  	e.printStackTrace();
+		  	}
+
+		  	strip_text = raw_text;
 
 	}
 
